9467ea7eb7aec4734224d94e74e4b726b68850aa
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2025-12-29 16:28:23 +0100

    Update ns_url_connection.rs

--- src/frameworks/foundation/ns_url_connection.rs
+++ src/frameworks/foundation/ns_url_connection.rs
@@ -20,6 +20,30 @@ pub const CLASSES: ClassExports = objc_classes! {
     autorelease(env, new)
 }
 
++ (id)connectionWithRequest:(id)request
+                   delegate:(id)delegate
+           startImmediately:(bool)start_immediately {
+    let new: id = msg![env; this alloc];
+    let new: id = msg![
+        env;
+        new initWithRequest:request
+                 delegate:delegate
+         startImmediately:start_immediately
+    ];
+    autorelease(env, new)
+}
+
++ (id)sendSynchronousRequest:(id)_request
+           returningResponse:(id *)_response
+                       error:(id *)_error {
+    log!("TODO: NSURLConnection sendSynchronousRequest");
+    nil
+}
+
++ (bool)canHandleRequest:(id)_request {
+    true
+}
+
 - (id)initWithRequest:(id)request // NSURLRequest *
              delegate:(id)delegate {
     msg![env; this initWithRequest:request delegate:delegate startImmediately:true]
@@ -36,6 +60,32 @@ pub const CLASSES: ClassExports = objc_classes! {
         start_immediately,
     );
     release(env, this);
+    nil
+}
+
+- (())start {
+    log!("TODO: [(NSURLConnection *){:?} start]", this);
+}
+
+- (())cancel {
+    log!("TODO: [(NSURLConnection *){:?} cancel]", this);
+}
+
+- (())scheduleInRunLoop:(id)_run_loop
+               forMode:(id)_mode {
+    // no-op
+}
+
+- (())unscheduleFromRunLoop:(id)_run_loop
+                    forMode:(id)_mode {
+    // no-op
+}
+
+- (id)description {
+    msg![env; super description]
+}
+
+- (id)currentRequest {
     nil
 }
 
