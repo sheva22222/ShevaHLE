048d54ee2df6e8943250684f8d4b02cd595cfebd
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2025-12-29 16:55:17 +0100

    Update ns_string.rs

--- src/frameworks/foundation/ns_string.rs
+++ src/frameworks/foundation/ns_string.rs
@@ -209,7 +209,8 @@ pub fn array_from_ids(env: &mut Environment, objects: &[id]) -> id {
     let ptr = objects.as_ptr();
     let len = objects.len();
 
-    let array: id = msg![env; NSArray alloc];
+    let array_class: id = msg_class![env; NSArray];
+    let array: id = msg![env; array_class alloc];
     let array: id = msg![env; array initWithObjects:ptr count:len];
     msg![env; array autorelease]
 }
