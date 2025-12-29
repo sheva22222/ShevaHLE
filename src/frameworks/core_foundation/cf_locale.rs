8ece175ff197dfa2ef4d8450edb7207acf9cd3c4
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2025-12-29 09:08:45 +0100

    Update cf_locale.rs

--- src/frameworks/core_foundation/cf_locale.rs
+++ src/frameworks/core_foundation/cf_locale.rs
@@ -110,7 +110,8 @@ fn CFLocaleCopyAvailableLocaleIdentifiers(env: &mut Environment) -> CFArrayRef {
     }
 
     let arr: id = msg_class![env; NSArray alloc];
-    msg![env; arr initWithObjects: &strings count: (strings.len() as NSUInteger)]
+    let count: NSUInteger = strings.len() as NSUInteger;
+    msg![env; arr initWithObjects:&strings count:count]
 }
 
 pub const FUNCTIONS: FunctionExports = &[
