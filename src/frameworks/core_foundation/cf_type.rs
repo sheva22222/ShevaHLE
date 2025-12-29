2cee583ea17fdd62cbebe0b5d2eff67682f95042
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2025-12-29 10:45:14 +0100

    Update cf_type.rs

--- src/frameworks/core_foundation/cf_type.rs
+++ src/frameworks/core_foundation/cf_type.rs
@@ -9,7 +9,7 @@ use super::{CFHashCode, CFIndex};
 use crate::dyld::{export_c_func, FunctionExports};
 use crate::frameworks::foundation::NSUInteger;
 use crate::objc::Class;
-use crate::{msg, nil, objc};
+use crate::{msg, objc};
 use crate::{msg_class, Environment};
 
 pub type CFTypeRef = objc::id;
@@ -45,65 +45,10 @@ pub fn CFHash(env: &mut Environment, object: CFTypeRef) -> CFHashCode {
     msg![env; object hash]
 }
 
-pub fn CFGetTypeID(_env: &mut Environment, object: CFTypeRef) -> usize {
-    if object.is_null() {
-        0
-    } else {
-        // Use class pointer identity as a stable type ID
-        unsafe { objc::object_getClass(object) as usize }
-    }
-}
-
-pub fn CFShow(env: &mut Environment, object: CFTypeRef) {
-    if object.is_null() {
-        log!("CFShow(NULL)");
-        return;
-    }
-    let desc: CFTypeRef = msg![env; object description];
-    log!("CFShow({:?})", desc);
-}
-
-pub fn CFCopyDescription(env: &mut Environment, object: CFTypeRef) -> CFTypeRef {
-    if object.is_null() {
-        return nil
-    }
-    let desc: CFTypeRef = msg![env; object description];
-    msg![env; desc copy]
-}
-
-pub fn CFGetAllocator(_env: &mut Environment, _object: CFTypeRef) -> CFTypeRef {
-    // Default allocator (treated as NULL/default elsewhere)
-    nil
-}
-
-pub fn CFMakeCollectable(_env: &mut Environment, object: CFTypeRef) -> CFTypeRef {
-    object
-}
-
-pub fn CFIsCollectable(_env: &mut Environment, _object: CFTypeRef) -> bool {
-    false
-}
-
-pub fn CFGetRetainCountNullable(env: &mut Environment, object: CFTypeRef) -> CFIndex {
-    if object.is_null() {
-        0
-    } else {
-        let count: NSUInteger = msg![env; object retainCount];
-        count as CFIndex
-    }
-}
-
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CFRetain(_)),
     export_c_func!(CFRelease(_)),
     export_c_func!(CFGetRetainCount(_)),
     export_c_func!(CFEqual(_, _)),
     export_c_func!(CFHash(_)),
-    export_c_func!(CFGetTypeID(_)),
-    export_c_func!(CFShow(_)),
-    export_c_func!(CFCopyDescription(_)),
-    export_c_func!(CFGetAllocator(_)),
-    export_c_func!(CFMakeCollectable(_)),
-    export_c_func!(CFIsCollectable(_)),
-    export_c_func!(CFGetRetainCountNullable(_)),
 ];
