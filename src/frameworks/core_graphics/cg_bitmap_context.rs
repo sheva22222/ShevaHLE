1cf562c0d09c77fd4c258b8bcd4efd68bc547511
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2025-12-31 16:48:20 +0100

    Update cg_bitmap_context.rs

--- src/frameworks/core_graphics/cg_bitmap_context.rs
+++ src/frameworks/core_graphics/cg_bitmap_context.rs
@@ -83,6 +83,7 @@ pub fn CGBitmapContextCreate(
         // TODO: is this the correct default?
         rgb_fill_color: (0.0, 0.0, 0.0, 0.0),
         transform: CGAffineTransformIdentity,
+        text_position: CGPoint,
         state_stack: Vec::new(),
     };
     let isa = env
