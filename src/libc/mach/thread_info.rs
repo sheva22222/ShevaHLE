6262474168d8fb2f9fbe083ed43fe998134c2b75
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-01-01 15:59:38 +0100

    Update thread_info.rs

--- src/libc/mach/thread_info.rs
+++ src/libc/mach/thread_info.rs
@@ -120,7 +120,7 @@ pub struct mach_msg_header_t {
     pub msgh_local_port: mach_port_t,
     pub msgh_reserved: u32,
     pub msgh_id: i32,
-    pub _private: (),
+    pub _private: u32,
 }
 unsafe impl SafeRead for mach_msg_header_t {}
 
