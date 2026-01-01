4ff726bdfe92e863e5d26775d25c8a3d05dd7d25
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-01-01 21:31:04 +0100

    Create cg_path.rs

---
+++ src/frameworks/core_graphics/cg_path.rs
@@ -0,0 +1,61 @@
+use crate::dyld::{export_c_func, FunctionExports};
+use crate::frameworks::core_foundation::{CFRelease, CFRetain};
+use crate::frameworks::core_graphics::CGPoint;
+use crate::objc::{objc_classes, ClassExports, HostObject};
+use crate::Environment;
+
+pub type CGPathRef = Ptr<c_void, false>;
+pub type CGMutablePathRef = Ptr<c_void, true>;
+
+#[derive(Default)]
+pub struct CGPathHostObject {
+    pub elements: Vec<PathElement>,
+}
+
+impl HostObject for CGPathHostObject {}
+
+#[derive(Clone)]
+pub enum PathElement {
+    MoveTo(CGPoint),
+    LineTo(CGPoint),
+    QuadCurveTo(CGPoint, CGPoint),
+    CurveTo(CGPoint, CGPoint, CGPoint),
+    CloseSubpath,
+}
+
+pub const CLASSES: ClassExports = objc_classes! {
+    (env, this, _cmd);
+
+    @implementation _touchHLE_CGPath: NSObject
+    @end
+};
+
+fn CGPathCreateMutable(env: &mut Environment) -> CGMutablePathRef {
+    let host_obj = Box::new(CGPathHostObject::default());
+
+    let class = env
+        .objc
+        .get_known_class("_touchHLE_CGPath", &mut env.mem);
+
+    env.objc.alloc_object(class, host_obj, &mut env.mem)
+}
+
+pub fn CGPathRetain(env: &mut Environment, path: CGPathRef) -> CGPathRef {
+    if !path.is_null() {
+        CFRetain(env, path)
+    } else {
+        path
+    }
+}
+
+pub fn CGPathRelease(env: &mut Environment, path: CGPathRef) {
+    if !path.is_null() {
+        CFRelease(env, path);
+    }
+}
+
+pub const FUNCTIONS: FunctionExports = &[
+    export_c_func!(CGPathCreateMutable()),
+    export_c_func!(CGPathRetain(_)),
+    export_c_func!(CGPathRelease(_)),
+];
