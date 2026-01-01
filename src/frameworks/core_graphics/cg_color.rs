6cca9008ac26c98c26af1cb4c218f04993c42f40
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-01-01 12:39:56 +0100

    Update cg_color.rs

--- src/frameworks/core_graphics/cg_color.rs
+++ src/frameworks/core_graphics/cg_color.rs
@@ -243,6 +243,7 @@ fn CGColorGetConstantColor(
 
 pub const kCGColorWhite: &str = "kCGColorWhite";
 pub const kCGColorBlack: &str = "kCGColorBlack";
+pub const kCGColorClear: &str = "kCGColorClear";
 
 pub const CONSTANTS: ConstantExports = &[
     (
