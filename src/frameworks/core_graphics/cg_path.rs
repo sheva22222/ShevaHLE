2f8765f6027e49baf3bd696717e89c6d4a49db94
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-01-01 21:43:48 +0100

    Update cg_path.rs

--- src/frameworks/core_graphics/cg_path.rs
+++ src/frameworks/core_graphics/cg_path.rs
@@ -1,7 +1,7 @@
 use crate::dyld::{export_c_func, FunctionExports};
 use crate::frameworks::core_foundation::{CFRelease, CFRetain};
-use crate::frameworks::core_graphics::CGPoint;
-use crate::mem::Ptr;
+use crate::frameworks::core_graphics::{CGFloat, CGPoint};
+use crate::mem::{ConstPtr, Ptr};
 use crate::objc::{objc_classes, ClassExports, HostObject};
 use crate::Environment;
 use std::ffi::c_void;
@@ -56,8 +56,61 @@ pub fn CGPathRelease(env: &mut Environment, path: CGPathRef) {
     }
 }
 
+fn CGPathMoveToPoint(
+    env: &mut Environment,
+    path: CGMutablePathRef,
+    _transform: ConstPtr<CGAffineTransform>,
+    x: CGFloat,
+    y: CGFloat,
+) {
+    let host = env
+        .objc
+        .borrow_mut::<CGPathHostObject>(path);
+
+    host.elements.push(PathElement::MoveTo(CGPoint { x, y }));
+}
+
+fn CGPathAddLineToPoint(
+    env: &mut Environment,
+    path: CGMutablePathRef,
+    _transform: ConstPtr<CGAffineTransform>,
+    x: CGFloat,
+    y: CGFloat,
+) {
+    let host = env
+        .objc
+        .borrow_mut::<CGPathHostObject>(path);
+
+    host.elements.push(PathElement::LineTo(CGPoint { x, y }));
+}
+
+fn CGPathAddCurveToPoint(
+    env: &mut Environment,
+    path: CGMutablePathRef,
+    _transform: ConstPtr<CGAffineTransform>,
+    cp1x: CGFloat,
+    cp1y: CGFloat,
+    cp2x: CGFloat,
+    cp2y: CGFloat,
+    x: CGFloat,
+    y: CGFloat,
+) {
+    let host = env
+        .objc
+        .borrow_mut::<CGPathHostObject>(path);
+
+    host.elements.push(PathElement::CurveTo(
+        CGPoint { x: cp1x, y: cp1y },
+        CGPoint { x: cp2x, y: cp2y },
+        CGPoint { x, y },
+    ));
+}
+
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CGPathCreateMutable()),
     export_c_func!(CGPathRetain(_)),
     export_c_func!(CGPathRelease(_)),
+    export_c_func!(CGPathMoveToPoint(_, _, _, _)),
+    export_c_func!(CGPathAddLineToPoint(_, _, _, _)),
+    export_c_func!(CGPathAddCurveToPoint(_, _, _, _, _, _, _)),
 ];
