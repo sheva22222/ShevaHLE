962137a7e58709e3439edb7c0d14c9f105e2cee0
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-01-02 04:33:14 +0100

    Update cg_path.rs

--- src/frameworks/core_graphics/cg_path.rs
+++ src/frameworks/core_graphics/cg_path.rs
@@ -1,6 +1,6 @@
 use crate::dyld::{export_c_func, FunctionExports};
 use crate::frameworks::core_foundation::{CFRelease, CFRetain};
-use crate::frameworks::core_graphics::{CGFloat, CGPoint};
+use crate::frameworks::core_graphics::{CGFloat, CGPoint, CGRect};
 use crate::frameworks::core_graphics::cg_affine_transform::CGAffineTransform;
 use crate::mem::{ConstPtr, Ptr};
 use crate::objc::{objc_classes, ClassExports, HostObject};
@@ -103,9 +103,91 @@ pub fn CGPathAddLines(
     }
 }
 
+pub fn CGPathCloseSubpath(
+    env: &mut Environment,
+    path: CGMutablePathRef,
+) {
+    if path.is_null() {
+        return;
+    }
+
+    let host = env
+        .objc
+        .borrow_mut::<CGPathHostObject>(path.cast());
+
+    host.elements.push(PathElement::CloseSubpath);
+}
+
+pub fn CGPathAddRect(
+    env: &mut Environment,
+    path: CGMutablePathRef,
+    transform: ConstPtr<CGAffineTransform>,
+    rect: CGRect,
+) {
+    if path.is_null() {
+        return;
+    }
+
+    let host = env
+        .objc
+        .borrow_mut::<CGPathHostObject>(path.cast());
+
+    let t = if transform.is_null() {
+        CGAffineTransform {
+            a: 1.0, b: 0.0,
+            c: 0.0, d: 1.0,
+            tx: 0.0, ty: 0.0,
+        }
+    } else {
+        env.mem.read(transform)
+    };
+
+    let min_x = rect.origin.x;
+    let min_y = rect.origin.y;
+    let max_x = rect.origin.x + rect.size.width;
+    let max_y = rect.origin.y + rect.size.height;
+
+    let p0 = t.apply_to_point(CGPoint { x: min_x, y: min_y });
+    let p1 = t.apply_to_point(CGPoint { x: max_x, y: min_y });
+    let p2 = t.apply_to_point(CGPoint { x: max_x, y: max_y });
+    let p3 = t.apply_to_point(CGPoint { x: min_x, y: max_y });
+
+    host.elements.push(PathElement::MoveTo(p0));
+    host.elements.push(PathElement::LineTo(p1));
+    host.elements.push(PathElement::LineTo(p2));
+    host.elements.push(PathElement::LineTo(p3));
+    host.elements.push(PathElement::CloseSubpath);
+}
+
+pub fn CGPathCreateCopy(
+    env: &mut Environment,
+    path: CGPathRef,
+) -> CGPathRef {
+    if path.is_null() {
+        return path;
+    }
+
+    let src = env
+        .objc
+        .borrow::<CGPathHostObject>(path.cast());
+
+    let host_obj = Box::new(CGPathHostObject {
+        elements: src.elements.clone(),
+    });
+
+    let class = env
+        .objc
+        .get_known_class("_touchHLE_CGPath", &mut env.mem);
+
+    env.objc.alloc_object(class, host_obj, &mut env.mem).cast()
+}
+
 pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CGPathCreateMutable()),
     export_c_func!(CGPathRetain(_)),
     export_c_func!(CGPathRelease(_)),
     export_c_func!(CGPathAddLines(_, _, _, _)),
+    export_c_func!(CGPathCloseSubpath(_)),
+    export_c_func!(CGPathAddRect(_, _, _)),
+    export_c_func!(CGPathCreateCopy(_)),
 ];
