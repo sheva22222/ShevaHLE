942e96cfab8337544e54690aa500dbc84220135f
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-01-04 20:11:58 +0100

    Update ui_web_view.rs

--- src/frameworks/uikit/ui_view/ui_web_view.rs
+++ src/frameworks/uikit/ui_view/ui_web_view.rs
@@ -7,7 +7,7 @@
 
 use crate::frameworks::foundation::ns_string::to_rust_string;
 use crate::msg;
-use crate::objc::{id, msg, nil, objc_classes, ClassExports};
+use crate::objc::{id, nil, objc_classes, ClassExports};
 use std::borrow::Cow;
 
 pub const CLASSES: ClassExports = objc_classes! {
