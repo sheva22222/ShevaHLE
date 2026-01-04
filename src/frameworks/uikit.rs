418c46c871f7057515a141c5ff0715d0f7c61a64
masterfanaedoodledash-cmd <masterfanaedoodledash@gmail.com>
2026-01-04 21:11:44 +0100

    Update uikit.rs

--- src/frameworks/uikit.rs
+++ src/frameworks/uikit.rs
@@ -24,6 +24,7 @@ pub mod ui_geometry;
 pub mod ui_graphics;
 pub mod ui_image;
 pub mod ui_image_picker_controller;
+pub mod ui_navigation_bar;
 pub mod ui_nib;
 pub mod ui_responder;
 pub mod ui_screen;
@@ -47,6 +48,7 @@ pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
         ui_image::CLASSES,
         ui_image_picker_controller::CLASSES,
         ui_nib::CLASSES,
+        ui_navigation_bar::CLASSES,
         ui_responder::CLASSES,
         ui_screen::CLASSES,
         ui_table_view::CLASSES,
