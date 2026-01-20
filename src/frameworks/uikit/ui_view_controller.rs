b228d47581d16ffa99a0daaa78ef5c20d3ce4661
whyineedname <emssaoig1@gmail.com>
2026-01-20 14:14:26 +0200

    Update ui_view_controller.rs

--- src/frameworks/uikit/ui_view_controller.rs
+++ src/frameworks/uikit/ui_view_controller.rs
@@ -11,6 +11,9 @@
 use crate::frameworks::core_graphics::CGRect;
 use crate::frameworks::foundation::ns_objc_runtime::NSStringFromClass;
 use crate::frameworks::foundation::ns_string::{from_rust_string, get_static_str, to_rust_string};
+use crate::frameworks::uikit::ui_application::{
+    UIInterfaceOrientation, UIInterfaceOrientationPortrait,
+};
 use crate::frameworks::uikit::ui_view::set_view_controller;
 use crate::objc::{
     id, msg, msg_class, nil, objc_classes, release, retain, Class, ClassExports, HostObject,
@@ -193,6 +196,10 @@ pub const CLASSES: ClassExports = objc_classes! {
     log_dbg!("[(UIViewController*){:?} setWantsFullScreenLayout:{}]", this, wants);
 }
 
+- (bool)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interface_orientation {
+    interface_orientation == UIInterfaceOrientationPortrait
+}
+    
 @end
 
 };
