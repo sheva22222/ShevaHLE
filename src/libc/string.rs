da4631d86e4935a122bef3169a1d0617f81f4bc4
whyineedname <emssaoig1@gmail.com>
2026-02-05 17:20:32 +0200

    Update string.rs

--- src/libc/string.rs
+++ src/libc/string.rs
@@ -141,7 +141,16 @@ fn memcpy(
     src: ConstVoidPtr,
     size: GuestUSize,
 ) -> MutVoidPtr {
-    GenericChar::<u8>::memcpy(env, dest.cast(), src.cast(), size).cast()
+    GenericChar::<u8>::memcpy(env, dest.cast(), src.cast(), size, GuestUSize::MAX).cast()
+}
+fn __memcpy_chk(
+    env: &mut Environment,
+    dest: MutVoidPtr,
+    src: ConstVoidPtr,
+    size: GuestUSize,
+    dest_size: GuestUSize,
+) -> MutVoidPtr {
+    GenericChar::<u8>::memcpy(env, dest.cast(), src.cast(), size, dest_size).cast()
 }
 fn memmove(
     env: &mut Environment,
@@ -556,6 +565,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(memset_pattern8(_, _, _)),
     export_c_func!(memset_pattern16(_, _, _)),
     export_c_func!(memcpy(_, _, _)),
+    export_c_func!(__memcpy_chk(_, _, _, _)),
     export_c_func!(memmove(_, _, _)),
     export_c_func!(__memmove_chk(_, _, _, _)),
     export_c_func!(memchr(_, _, _)),
