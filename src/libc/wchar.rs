9fab4e217e877752812d84839cc29b0f4cf1289a
whyineedname <emssaoig1@gmail.com>
2026-02-05 17:18:09 +0200

    Update wchar.rs

--- src/libc/wchar.rs
+++ src/libc/wchar.rs
@@ -67,7 +67,16 @@ fn wmemmove(
     src: ConstPtr<wchar_t>,
     size: GuestUSize,
 ) -> MutPtr<wchar_t> {
-    GenericChar::<wchar_t>::memmove(env, dest, src, size)
+    GenericChar::<wchar_t>::memmove(env, dest, src, size, GuestUSize::MAX)
+}
+fn __wmemmove_chk(
+    env: &mut Environment,
+    dest: MutPtr<wchar_t>,
+    src: ConstPtr<wchar_t>,
+    size: GuestUSize,
+    dest_size: GuestUSize,
+) -> MutPtr<wchar_t> {
+    GenericChar::<wchar_t>::memmove(env, dest, src, size, dest_size)
 }
 fn wmemchr(
     env: &mut Environment,
@@ -171,6 +180,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(wmemset(_, _, _)),
     export_c_func!(wmemcpy(_, _, _)),
     export_c_func!(wmemmove(_, _, _)),
+    export_c_func!(__wmemmove_chk(_, _, _, _)),
     export_c_func!(wmemchr(_, _, _)),
     export_c_func!(wmemcmp(_, _, _)),
     export_c_func!(wcslen(_)),
