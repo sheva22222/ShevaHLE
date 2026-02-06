d292d7394004ff664d0a68cd40305952a51df2cb
whyineedname <emssaoig1@gmail.com>
2026-02-06 23:50:27 +0200

    Update ui_text_field.rs

--- src/frameworks/uikit/ui_view/ui_control/ui_text_field.rs
+++ src/frameworks/uikit/ui_view/ui_control/ui_text_field.rs
@@ -132,6 +132,15 @@ pub const CLASSES: ClassExports = objc_classes! {
 - (())setText:(id)text { // NSString*
     let text_label = env.objc.borrow_mut::<UITextFieldHostObject>(this).text_label;
     () = msg![env; text_label setText:text];
+    // This will work only if all the text changes will call setText:!
+    // This is the case right now.
+    // (see `handle_text` and `handle_backspace` helper functions below)
+    // TODO: actually check if setText: send this notif on each change
+    // (e.g. does it send the notif if text hasn't changed)
+    let center: id = msg_class![env; NSNotificationCenter defaultCenter];
+    let name = ns_string::get_static_str(env, UITextFieldTextDidChangeNotification);
+    // TODO: userInfo
+    let _: () = msg![env; center postNotificationName:name object:this userInfo:nil];
 }
 
 - (())setTextColor:(id)color { // UIColor*
