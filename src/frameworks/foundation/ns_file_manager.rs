5d6480ac3549fd2369df3c57e53eda777a48e2bf
whyineedname <emssaoig1@gmail.com>
2026-02-06 23:48:52 +0200

    Update ns_file_manager.rs

--- src/frameworks/foundation/ns_file_manager.rs
+++ src/frameworks/foundation/ns_file_manager.rs
@@ -193,6 +193,23 @@ pub const CLASSES: ClassExports = objc_classes! {
     }
 }
 
+- (bool)moveItemAtPath:(id)path // NSString*
+                toPath:(id)toPath // NSString*
+                 error:(MutPtr<id>)error { // NSError**
+    // TODO: call delegate
+    let path = ns_string::to_rust_string(env, path); // TODO: avoid copy
+    let toPath = ns_string::to_rust_string(env, toPath); // TODO: avoid copy
+    match env.fs.rename(GuestPath::new(&path), GuestPath::new(&toPath)) {
+        Ok(()) => true,
+        Err(()) => {
+            if !error.is_null() {
+               todo!(); // TODO: create an NSError if requested
+            }
+            false
+        }
+    }
+}
+    
 - (bool)createDirectoryAtPath:(id)path // NSString *
                    attributes:(id)attributes { // NSDictionary*
     let error: MutPtr<id> = Ptr::null();
